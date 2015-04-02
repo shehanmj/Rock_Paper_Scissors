04fc0a52d092e16b17eb432cce37e72d6616c3b0
Jason <shehanmj@yahoo.com>
2015-04-02 18:48:46 -0500

    new file:   rock_paper_scissors.js A very basic else/if compare javascript game.

---
+++ rock_paper_scissors.js
@@ -0,0 +1,46 @@
+﻿var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+computerChoice = "paper";
+} else {
+computerChoice = "scissors";
+} console.log("Computer: " + computerChoice);
+
+var compare = function(choice1, choice2) {
+if(choice1 === choice2) {
+return "The result is a tie"
+}
+
+else if(choice1 === "rock") {
+
+if(choice2 === "scissors") {
+    return "rock wins";
+
+} else if (choice2 === "paper") {
+    return "paper wins";
+}
+}
+
+
+if (Choice1 === "paper") {
+if (Choice2 === "rock") {
+return "paper wins";
+}
+else if (Choice2 === "scissors") {
+return "scissors win";
+}
+}
+
+if (Choice1 === "scissors") {
+if (Choice2 === "rock") {
+return "rock wins";
+}
+else if (Choice2 === "paper") {
+return "scissors win";
+}
+}
+}
+console.log("User:" + userChoice )
+console.log(compare(userChoice, computerChoice))
